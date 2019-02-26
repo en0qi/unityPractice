5d84d04df5a94b571653e11d2c26c929f1626c3c
en0qi <ynoaki@me.com>
2019-02-26 12:04:01 +0900

    lookAtPlayer

---
+++ Assets/unity-chan!/Unity-chan! Model/follow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+
+public class follow : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset;
+        
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.transform.position;    
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+        transform.LookAt(player.transform.position);
+        transform.RotateAround(player.transform.position, Vector3.up, player.transform.localEulerAngles.y + 180);
+        //transform.rotation = Quaternion.Euler(0, player.transform.localEulerAngles.y +180, 0);
+    }
+}
